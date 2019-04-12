28323230cb2683a6e75b9812183106213f9e2ea5
Niall Moore <niall@hycon.io>
2019-04-12 13:29:28 +0900

    Added Typescript Definitions

---
+++ lib/index.d.ts
@@ -0,0 +1,5 @@
+export as namespace DifficultyAdjuster;
+export function adjust(workEMA: number, timeEMA: number): number;
+export function ema(newValue: number, previousValue: number, alpha: number);
+
+
