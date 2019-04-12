fbb5362f687af1f720e26f5127118e3a551f3136
Niall Moore <niall@hycon.io>
2019-04-12 13:42:03 +0900

    Updated type defs

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,8 @@
+/** Nodejs wrapper for Hycon difficulty adjustment written in Rust */
 export as namespace DifficultyAdjuster;
+/** Adjsuts the difficulty target for the next block based on the EMA parameters provided */
 export function adjust(workEMA: number, timeEMA: number): number;
-export function ema(newValue: number, previousValue: number, alpha: number);
+/** Calculates a new EMA value */
+export function ema(newValue: number, previousValue: number, alpha: number): number;
 
 
