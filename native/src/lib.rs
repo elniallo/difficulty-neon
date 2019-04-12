5dab8483a3f7d62380c32f7b08e5df036e04bd9e
Niall Moore <niall@hycon.io>
2019-04-12 10:44:43 +0900

    Added EMA calculation function

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -11,7 +11,14 @@ fn adjust(mut cx: FunctionContext) -> JsResult<JsNumber> {
         let calc: f64 = 1.0 - 0.5_f64.powf(val);
     Ok(cx.number(calc))
 }
+fn calc_ema(mut cx: FunctionContext) -> JsResult<JsNumber> {
+    let new_value: f64 = cx.argument::<JsNumber>(0)?.value();
+    let previous_value: f64 = cx.argument::<JsNumber>(1)?.value();
+    let alpha: f64 = cx.argument::<JsNumber>(2)?.value();
+    Ok(cx.number(alpha * new_value + (1.0 - alpha) * previous_value))
+}
 
 register_module!(mut cx, {
-    cx.export_function("adjust", adjust)
+    cx.export_function("adjust", adjust);
+    cx.export_function("ema",calc_ema)
 });
