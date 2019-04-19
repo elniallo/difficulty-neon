f46510f8082acf2283a5ef608d7c2e8c26cee861
Niall Moore <niall@hycon.io>
2019-04-19 14:43:24 +0900

    update calculation

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -7,16 +7,16 @@ fn adjust(mut cx: FunctionContext) -> JsResult<JsNumber> {
         let work_ema: f64 = cx.argument::<JsNumber>(0)?.value();
         let t_ema: f64 = cx.argument::<JsNumber>(1)?.value();
         let hash_perc: f64 = work_ema / t_ema;
-        let val: f64 = 1.0 / (15000.0 * hash_perc);
-        let calc: f64 = 1.0 - 0.5_f64.powf(val);
+        let calc: f64 = 1.0 - 0.5_f64.powf(t_ema / (15000.0 * work_ema));
     Ok(cx.number(calc))
 }
 fn calc_ema(mut cx: FunctionContext) -> JsResult<JsNumber> {
     let new_value: f64 = cx.argument::<JsNumber>(0)?.value();
     let previous_value: f64 = cx.argument::<JsNumber>(1)?.value();
     let alpha: f64 = cx.argument::<JsNumber>(2)?.value();
-    Ok(cx.number(alpha * new_value + (1.0 - alpha) * previous_value))
-}
+    let res = alpha.mul_add(new_value,((1.0 - alpha) * previous_value));
+    Ok(cx.number(res))
+    }
 
 register_module!(mut cx, {
     cx.export_function("adjust", adjust);
